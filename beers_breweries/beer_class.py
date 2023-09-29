a9c2661f607a9c8fccc1f6f235ea0194c828bf19
rbedker <137824541+rbedker@users.noreply.github.com>
2023-09-29 13:41:30 -0500

    Update beer_class.py

--- beers_breweries/beer_class.py
+++ beers_breweries/beer_class.py
@@ -1,23 +1,27 @@
-class Beer:
+class Beer():
     """ 
-    This class creates a beer object with the __init__ function needing(brewery_id, name, style, abv) 
+    This class creates a beer object with the __init__ function needing(brewery_id, beer_id, name, style, abv) 
+    Methods include 
+    get_brewery_id()
+    get_beer_id()
+    get_beer_info()
     """
-    
-    def __init__(self, brewery_id, name, style, abv, beer_id):
+    def __init__(self, brewery_id, beer_id, name, style, abv):
         self.brewery_id = brewery_id
+        self.beer_id = beer_id
         self.name = name
         self.style = style
         self.abv = abv
-        self.id = beer_id
-        self.brewery = None
         
+    def get_brewery_id(self):
+        return self.brewery_id
     
-    def
-        
+    def get_beer_id(self):
+        return self.beer_id
     
-    def __str__(self):
-        return f"
-    
+    def get_beer_info(self):
+        return f'{self.name} {self.style}'
+
     
     if __name__ == '__main__':
         print("Running for debug")
