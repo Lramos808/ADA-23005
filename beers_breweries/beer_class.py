fddd88e0d8d9e74da6515d9055cf9c06cd8d2bb6
hccpu <andrewmcornejo@gmail.com>
2023-09-29 18:15:37 +0000

    Added the py and the end if thing

---
+++ beers_breweries/beer_class.py
@@ -0,0 +1,14 @@
+class Beer():
+    """ 
+    This class creates a beer object with the __init__ function needing(brewery_id, name, style, abv) 
+    """
+    
+    def __init__(self, brewery_id, name, style, abv, beer_id):
+        self.brewery_id = brewery_id
+        self.name = name
+        self.style = style
+        self.abv = abv
+        self.id = beer_id
+    
+    if __name__ == '__main__':
+        print("Running for debug")
