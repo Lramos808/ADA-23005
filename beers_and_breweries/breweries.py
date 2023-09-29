7361c6f2abecc98268e5a29dc9e7cd8acf660df2
tavros669 <jan.tsiklauri@us.af.mil>
2023-09-29 18:38:23 +0000

    blah

--- beers_and_breweries/breweries.py
+++ beers_and_breweries/breweries.py
@@ -1,14 +1,16 @@
 class Breweries:
     def __init__(self, data):
         self.b_id = data[0]
-        self.name = name[1]
+        self.name = data[1]
         self.city = data[2]
-        self.state = state[3]
+        self.state = data[3]
         self.beers = []
         
     
     def add_beer(self, beer):
         self.beers.append(beer)
+        
+        # beer.brewery = self
         
     
     def list_beers(self):
