6e19c870f733694edf4668b5ca3b60317391b359
rbedker <137824541+rbedker@users.noreply.github.com>
2023-10-04 13:32:11 -0500

    Update main.py
    good enough

--- euler_67/bedkercordua/main.py
+++ euler_67/bedkercordua/main.py
@@ -9,24 +9,23 @@ with open("triangle.txt", "r", newline = "\n") as f:
             for num in number:
                 num = int(num)
                 number_list.append(num)
-            triangle_list.insert(0,number_list)
+            triangle_list.insert(0, number_list)
 
 obj_list = []
+
 for number_list in triangle_list:
+    obj1_list = []
     for num in number_list:
-        obj1_list = []
-        obj1_list.append(Node(num))
-        obj_list.append(obj1_list)
+        obj1_list.append(Node(int(num)))
+    obj_list.append(obj1_list)
 
+
+for object_row in range(1, len(obj_list)):
+    for object_colum in range(0, len(obj_list[object_row])):
+        obj_list[object_row][object_colum].set_left(obj_list[object_row-1][object_colum])
+        obj_list[object_row][object_colum].set_right(obj_list[object_row-1][object_colum+1])
+        obj_list[object_row][object_colum].determine_greater()
 
         
-           
+obj_list[99][0].get_sum_value()        
             
-counter = len(triangle_list)
-
-for object_row in range(0, 100):
-    for object_colum in range(0, 100):
-        print(object_row)
-        print(object_colum)
-        print(obj_list[object_row][object_colum].get_value())
-        
