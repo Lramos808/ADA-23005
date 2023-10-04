66757ba4c94c2a5d077b46a0e4b213c7b2a3f4ee
A1C Ramos <jupyter-lramos@ip-172-31-24-158.us-east-2.compute.internal>
2023-10-04 13:15:49 +0000

    node add

---
+++ euler_67/node.py
@@ -0,0 +1,27 @@
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+    def get_value(self):
+        return self.value
+    
+    def set_value(self, value):
+        self.value = value
+        
+    def get_left(self):
+        return self.left
+    
+    def set_left(self, left):
+        self.left = left
+        
+    def get_right(self):
+        return self.right
+    
+    def set_right(self, right):
+        self.right = right
+        
+
+        
