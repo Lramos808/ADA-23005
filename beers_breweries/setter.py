01a57e32f2e2f3b40d01f0192d957e8f43a3ee85
tcordua <146353112+tcordua@users.noreply.github.com>
2023-10-06 08:46:34 -0500

    Create setter.py

---
+++ beers_breweries/setter.py
@@ -0,0 +1,15 @@
+def setter(beer, brewery_list):
+    '''
+    place beer object in brewery object
+    
+    look through brewery beer list and add the brewery to the beer's brewery list
+    '''
+    
+        
+    brewery_list[beer.brewery_id].beer_list.append(beer)
+
+    beer.brewery = brewery_list[beer.brewery_id]
+    
+return beer, brewery_list
+
+        
