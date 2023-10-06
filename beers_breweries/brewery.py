475bf7886d9d689eb5a1e9368eb1a8e519a56514
rbedker <137824541+rbedker@users.noreply.github.com>
2023-10-06 08:01:50 -0500

    Create brewery.py
    creating brewery objects with a brewery dataclass

---
+++ beers_breweries/brewery.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass, field
+
+@dataclass (order=True)
+
+class Breweries:
+  """ 
+  This dataclass takes a list with 3 freatures; name, city and state. The brewery object instanciated will also have an empty beer_list
+  The dataclass also contains one method, add_beer_obj(self, beer) there it takes a beer object and appends to list
+  """
+    brewery_id: int
+    name: str
+    city: str
+    state: str
+    beer_list: list = field(default_factory=list)
+    
+    def add_beer_obj(self, beer):
+        self.beer_list.append(beer)
+    
+    def __str__(self):
+        return f"{self.name} {self.city} {self.state}"
+
+""" The following code imports the breweries.csv and creates a list of brewery objects called brewery_obj"""
+
+if __name__ == "__main__":
+    brewery_obj = []
+    with open("breweries.csv", "r") as f:
+        for line in f:
+            line = line.replace("\n", "")
+            line = line.split(",")
+            brewery_obj.append(Breweries(*line))
+            
+    brewery_obj.pop(0)
